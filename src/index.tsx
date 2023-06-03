ef86f209aae988de2f6e6167c627946ee771f232
solch4 <sol.maldonado.0209@gmail.com>
2023-06-03 01:29:34 -0300

    redux toolkit added

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
+import axios from "axios";
 import './index.css';
-import App from './App';
+import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import store from "./app/store";
+
+axios.defaults.baseURL = "https://api.mockfly.dev/mocks/b0cdca18-facb-4a9f-a943-5154f890cb80/api"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
