6cb52eb35d55fbfd175331d4671a7f7695cf4491
solch4 <sol.maldonado.0209@gmail.com>
2023-06-07 02:09:56 -0300

    DestinationDetail styles added

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ const Layout: React.FC<Props> = ({ children }: Props) => {
   return (
     <div className="lg:grid lg:grid-cols-4">
       <Header />
-      <main className="lg:col-span-3 grid gap-4 lg:gap-8 p-4 lg:px-6 lg:py-10">
+      <main className="lg:col-span-3 lg:h-min grid gap-4 lg:gap-8 p-4 lg:px-6 lg:py-10">
         {children}
       </main>
     </div>
--- src/pages/DestinationDetail.tsx
+++ src/pages/DestinationDetail.tsx
@@ -1,24 +1,51 @@
 import { useDestinationDetail } from "../hooks/useDestinationDetail";
+import Star from "../components/Star";
 
 const DestinationDetail = () => {
   const { destination, error, loading } = useDestinationDetail();
+  const { name, image, description, rating, location } = destination;
+
+  const nOfRating: number[] = [];
+  for (let i = 1; i <= Math.round(rating); i++) nOfRating.push(i);
 
   if (loading) return <h1>Cargando detalles...</h1>;
   if (error) return <h1>Algo salió mal: {error}</h1>;
 
   return (
     <>
-      <h1>Destino</h1>
-      {destination && (
-        <div key={destination.id}>
-          <img src={destination.image} alt={destination.name} />
-          <h1>{destination.name}</h1>
-          <p>{destination.description}</p>
-          <p>{destination.rating}</p>
-          <p>{destination.category}</p>
-          <p>{destination.location}</p>
+      {/* imagen */}
+      <div className="h-56 lg:h-96">
+        <img
+          className="w-full h-full object-cover rounded-2xl"
+          src={image}
+          alt={name}
+        />
+      </div>
+      {/* detalles */}
+      <div className="lg:grid lg:grid-cols-3 lg:gap-6">
+        <div className="lg:col-span-2 space-y-2 lg:space-y-6">
+          <h1 className="text-neutral-900 text-3xl lg:text-4xl font-bold">
+            {name}
+          </h1>
+          <p className="text-neutral-800 text-2xl lg:text-3xl font-bold">
+            {location}
+          </p>
+          <p>{description}</p>
         </div>
-      )}
+        {/* rating */}
+        <div className="justify-self-end self-start mt-2 lg:mt-0 flex items-center gap-2">
+          {/* stars */}
+          <div className="flex gap-[2px]" aria-hidden="true">
+            {nOfRating.map((n) => (
+              <Star className="h-6" key={n} />
+            ))}
+          </div>
+          <p className="text-xl lg:text-2xl font-bold">
+            <span className="sr-only">Rating:</span>
+            {rating}
+          </p>
+        </div>
+      </div>
     </>
   );
 };
