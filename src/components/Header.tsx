3a8ae77c607125e3f03c1bb901d31dafad0baa6f
solch4 <sol.maldonado.0209@gmail.com>
2023-06-07 00:46:41 -0300

    Header styles added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useWindowSize } from "../hooks/useWindowSize";
 
 const Header = () => {
+  const windowSize = useWindowSize();
+  const [isOpen, setOpen] = useState(false);
+  const isLargeScreen = () => windowSize.width >= 1024;
+
   return (
-    <header>
-      <h1>
-        <Link to="/">Travel Explorer</Link>
-      </h1>
+    <header className="bg-neutral-50 sticky top-0 z-30 lg:h-screen">
+      <div className="flex justify-between p-4 lg:px-6 lg:py-10">
+        <h1 className="text-neutral-900 text-2xl lg:text-3xl font-bold">
+          <Link to="/">Travel Explorer</Link>
+        </h1>
+        <button
+          className="lg:hidden px-2"
+          onClick={() => setOpen(!isOpen)}
+          aria-label="Menu"
+        >
+          {isOpen ? "X" : "☰"}
+        </button>
+      </div>
+      {/* menú desplegable en mobile / navegación en desktop */}
+      {(isOpen || isLargeScreen()) && (
+        <ul className="bg-neutral-50 absolute w-full py-6 px-4 lg:px-6 lg:py-0">
+          <li>
+            <Link
+              to="/create"
+              className="text-lg lg:text-xl text-neutral-900 font-bold"
+            >
+              Agregar destino
+            </Link>
+          </li>
+        </ul>
+      )}
     </header>
   );
 };
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,10 +6,10 @@ interface Props {
 
 const Layout: React.FC<Props> = ({ children }: Props) => {
   return (
-    <>
+    <div className="lg:grid lg:grid-cols-4 lg:gap-6">
       <Header />
-      <main>{children}</main>
-    </>
+      <main className="lg:col-span-3">{children}</main>
+    </div>
   );
 };
 
