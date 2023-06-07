b296dc7603604f453b9daf916da9d328e1ece3e5
solch4 <sol.maldonado.0209@gmail.com>
2023-06-07 13:46:22 -0300

    CreateDestination styles added

--- src/pages/CreateDestination.tsx
+++ src/pages/CreateDestination.tsx
@@ -9,9 +9,22 @@ const CreateDestination = () => {
 
   return (
     <>
-      <h1>Agregar nuevo destino</h1>
+      <h1 className="text-neutral-900 text-3xl lg:text-4xl font-bold">
+        Agregar nuevo destino
+      </h1>
       <p>Llena el formulario con la información requerida.</p>
-      <CreateDestinationForm />
+      <div className="lg:grid lg:grid-cols-9 lg:gap-6 lg:items-center">
+        <div className="lg:col-span-4">
+          <CreateDestinationForm />
+        </div>
+        <div className="hidden lg:block lg:col-span-5">
+          <img
+            className="w-full"
+            src="./assets/illustration.svg"
+            alt="Ilustración de una mujer caminando en el bosque"
+          />
+        </div>
+      </div>
     </>
   );
 };
