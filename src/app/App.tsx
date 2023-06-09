b64cf415c8f6b200dda27e979873a196d36ad067
solch4 <sol.maldonado.0209@gmail.com>
2023-06-09 02:44:50 -0300

    lazy loading added to routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,25 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "../components/Layout";
-import Home from "../pages/Home";
-import CreateDestination from "../pages/CreateDestination";
-import DestinationDetail from "../pages/DestinationDetail";
-import NotFound from "../pages/NotFound";
+import Loader from "../components/Loader";
+
+const Home = lazy(() => import("../pages/Home"));
+const CreateDestination = lazy(() => import("../pages/CreateDestination"));
+const DestinationDetail = lazy(() => import("../pages/DestinationDetail"));
+const NotFound = lazy(() => import("../pages/NotFound"));
 
 const App = () => {
   return (
     <BrowserRouter>
       <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create" element={<CreateDestination />} />
-          <Route path="/:destinationId" element={<DestinationDetail />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<Loader>Cargando...</Loader>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreateDestination />} />
+            <Route path="/:destinationId" element={<DestinationDetail />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </Layout>
     </BrowserRouter>
   );
