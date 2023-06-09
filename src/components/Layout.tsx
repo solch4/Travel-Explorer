bd97a04ad1b1a7cc2781f72a8b113115dc5b50b6
solch4 <sol.maldonado.0209@gmail.com>
2023-06-09 04:37:52 -0300

    skip to main content link added

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,8 +7,17 @@ interface Props {
 const Layout: React.FC<Props> = ({ children }: Props) => {
   return (
     <div className="lg:grid lg:grid-cols-4">
+      <a
+        className="-translate-y-full focus:translate-y-0 bg-primary-700 text-primary-50 font-semibold fixed z-50 p-2 transition"
+        href="#main"
+      >
+        Saltar al contenido principal
+      </a>
       <Navbar />
-      <main className="lg:col-span-3 lg:h-min grid gap-4 lg:gap-8 p-4 lg:px-6 lg:py-10">
+      <main
+        className="lg:col-span-3 lg:h-min grid gap-4 lg:gap-8 p-4 lg:px-6 lg:py-10"
+        id="main"
+      >
         {children}
       </main>
     </div>
