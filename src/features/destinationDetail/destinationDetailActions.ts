7c8294eb944d89308a857142ba959becb93fc4aa
solch4 <sol.maldonado.0209@gmail.com>
2023-08-16 16:13:17 -0300

    getDetail updated

--- src/features/destinationDetail/destinationDetailActions.ts
+++ src/features/destinationDetail/destinationDetailActions.ts
@@ -6,8 +6,13 @@ export const getDetail = createAsyncThunk(
   "destinations/getDetail",
   async (id: string) => {
     try {
-      const { data } = await axios.get<Destination>(`/destinations/${id}`);
-      return data;
+      // al usar una API real se cambiaría la url y respuesta
+      const { data } = await axios.get<Destination[]>(`/destinations`);
+      const destinationDetail = data.find(
+        (destination) => destination.id === id
+      );
+      if (!destinationDetail) throw new Error("Error al obtener detalles");
+      return destinationDetail;
     } catch (error) {
       console.error(error);
       throw new Error("Error al obtener detalles");
