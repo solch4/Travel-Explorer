4f29590ebcd8569a42aa301599c73df38f635bf4
solch4 <sol.maldonado.0209@gmail.com>
2023-08-26 17:13:37 -0300

    Navbar updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useWindowSize } from "../hooks/useWindowSize";
-import NavbarItem from "./NavbarItem";
+
+const navLinks = [
+  { path: "/", label: "Inicio" },
+  { path: "/create", label: "Agregar destino" },
+];
 
 const Navbar = () => {
   const windowSize = useWindowSize();
@@ -10,7 +14,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-primary-100 text-neutral-800 sticky top-0 z-30 lg:h-screen">
-      <div className="flex justify-between p-4 lg:px-6 lg:py-10">
+      <div className="flex justify-between p-4 lg:px-6 lg:py-10 z-30">
         <h1 className="text-neutral-900 text-2xl lg:text-3xl font-bold">
           <Link to="/">Travel Explorer</Link>
         </h1>
@@ -24,10 +28,25 @@ const Navbar = () => {
       </div>
       {/* menú desplegable en mobile / navegación en desktop */}
       {(isOpen || isLargeScreen()) && (
-        <ul className="bg-primary-100 absolute w-full py-6 px-4 lg:px-6 lg:py-0 space-y-4">
-          <NavbarItem path="/" label="Inicio" />
-          <NavbarItem path="/create" label="Agregar destino" />
-        </ul>
+        <>
+          <ul className="bg-primary-100 absolute z-30 w-full py-6 px-4 lg:px-6 lg:py-0 space-y-4">
+            {navLinks.map((link) => (
+              <li key={link.label}>
+                <Link
+                  className="text-lg lg:text-xl font-bold"
+                  onClick={() => setOpen(!isOpen)}
+                  to={link.path}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div
+            className="lg:hidden h-screen absolute top-full w-full bg-neutral-900/50 backdrop-blur-sm z-20"
+            onClick={() => setOpen(!isOpen)}
+          />
+        </>
       )}
     </nav>
   );
